ad92e8205f9429e3a6b3a2b565f13a8adf4ec3ed
Namanbukday <nbukday@gamil.com>
2019-07-11 07:57:13 +0530

    imported settings class and used its instance

--- ch12_alien_invasion.py
+++ ch12_alien_invasion.py
@@ -7,15 +7,24 @@
 import sys
 import pygame
 
+# FROM SETTINGS.PY, WE IMPORT THE SETTINGS CLASS
+from ch12_settings import Settings
+
 
 def run_game():
     # initialize game nad create the 'screen' object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+
+    # MAKING AN INSTANCE OF SETTINGS CLASS TO USE ITS ATTRIBUTES
+    ai_settings = Settings()
+
+    # WE USE ATTRIBUTES OF ai_settings LIKE SCREEN_HEIGHT AND SCREEN_WIDTH TO SET SCREEN COLOR
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+
     pygame.display.set_caption("ALIEN INVASION")
 
     # set background color
-    bg_color = (127, 255, 212)  # aquamarine
+    # bg_color = (127, 255, 212)  # aquamarine
 
     # starts the main game controlling loop for the game
     while True:
@@ -26,7 +35,7 @@ def run_game():
 
         # Redraw screen during each pass through the loop
         # And setting the screen color
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)  # USED INSTANCE TO CHANGE THE COLOR
 
         # make the most recently drawn screen visible
         pygame.display.flip()
